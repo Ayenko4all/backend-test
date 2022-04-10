8e5a6ea6582b609c1fb5dbc364858a3979b5ebc9
Ayeni kehinde Oluwatosin <kenny4all40@gmail.com>
2022-04-10 06:34:57 +0100

    update: moved routes to api route path

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,3 @@ use App\Http\Controllers\LessonController;
 use Illuminate\Support\Facades\Route;
 
 
-//Route::post('comments', [CommentController::class, 'store'])->name('comment.store');
-//Route::get('/users/{user}/achievements', AchievementsController::class)->name('user.achievements');
-//Route::post('lessons/{lesson}/{user}',LessonController::class)->name('lesson.watched');
